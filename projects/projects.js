ba8589d0317fcbb6e14225959a0ba1ab9f477a4a
Ankita Inamti <ankita.inamti@gmail.com>
2025-10-25 11:49:22 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -2,3 +2,32 @@ import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
+
+export function renderProjects(project, containerElement) {
+  containerElement.innerHTML = '';
+  projects.forEach(project => {
+  const article = document.createElement('article');
+  article.innerHTML = `
+    <h3>${project.title}</h3>
+    <img src="${project.image}" alt="${project.title}">
+    <p>${project.description}</p>
+`;
+containerElement.appendChild(article);
+
+  })
+
+}
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+  containerElement.innerHTML = '';
+  projects.forEach(project => {
+  const article = document.createElement('article');
+  article.innerHTML = `
+      <${headingLevel}>${project.title}</${headingLevel}>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
+containerElement.appendChild(article);
+
+  })
+}
