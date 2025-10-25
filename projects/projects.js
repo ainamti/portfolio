165415c5f4a386b271eae1a0dc8198fc61149ded
Ankita Inamti <ankita.inamti@gmail.com>
2025-10-25 12:03:20 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,7 +1,6 @@
 import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
-renderProjects(projects, projectsContainer, 'h2');
 
 export function renderProjects(project, containerElement) {
   containerElement.innerHTML = '';
