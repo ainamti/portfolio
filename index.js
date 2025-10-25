3ab3fbacc8469a87a11c8da1619214b8f13dd792
Ankita Inamti <ankita.inamti@gmail.com>
2025-10-25 15:40:08 -0700

    Update index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
+async function displayLatestProjects() {
+  // Fetch all projects
+  const projects = await fetchJSON('./lib/projects.json');
+
+  // Take only the first 3
+  const latestProjects = projects.slice(0, 3);
+
+  // Select container on homepage
+  const projectsContainer = document.querySelector('.projects');
+
+  // Safety check
+  if (!projectsContainer) {
+    console.error('No .projects container found in index.html');
+    return;
+  }
+
+  // Render first 3 projects
+  renderProjects(latestProjects, projectsContainer, 'h3');
+}
+
+displayLatestProjects();
+
+renderProjects(latestProjects, projectsContainer, 'h2');
+<script src="index.js" type="module" defer></script>
