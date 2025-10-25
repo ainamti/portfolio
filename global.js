e0e6a12eb74678f68faf2d4fec1310a24ccf703f
Ankita Inamti <ankita.inamti@gmail.com>
2025-10-25 12:35:05 -0700

    fix

--- global.js
+++ global.js
@@ -98,25 +98,24 @@ export async function fetchJSON(url) {
     }
     const data = await response.json();
     return data;
-    } 
-    catch (error) {
+  } 
+  catch (error) {
     console.error('Error fetching or parsing JSON data:', error);
   }
 }
 
-export function renderProjects(project, containerElement, headingLevel) {
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
   containerElement.innerHTML = '';
+  
   projects.forEach(project => {
-  const article = document.createElement('article');
-  article.innerHTML = `
+    const article = document.createElement('article');
+    article.innerHTML = `
       <${headingLevel}>${project.title}</${headingLevel}>
       <img src="${project.image}" alt="${project.title}">
       <p>${project.description}</p>
     `;
-  containerElement.appendChild(article);
-
-  })
-
+    containerElement.appendChild(article);
+  });
 }
 
 
--- projects/projects.js
+++ projects/projects.js
@@ -1,33 +1,10 @@
 import { fetchJSON, renderProjects } from '../global.js';
+
+// Fetch the projects data
 const projects = await fetchJSON('../lib/projects.json');
+
+// Get the container element
 const projectsContainer = document.querySelector('.projects');
+
+// Render the projects with h2 headings
 renderProjects(projects, projectsContainer, 'h2');
-
-export function renderProjects(project, containerElement) {
-  containerElement.innerHTML = '';
-  projects.forEach(project => {
-  const article = document.createElement('article');
-  article.innerHTML = `
-    <h3>${project.title}</h3>
-    <img src="${project.image}" alt="${project.title}">
-    <p>${project.description}</p>
-`;
-containerElement.appendChild(article);
-
-  })
-
-}
-
-export function renderProjects(project, containerElement, headingLevel = 'h2') {
-  containerElement.innerHTML = '';
-  projects.forEach(project => {
-  const article = document.createElement('article');
-  article.innerHTML = `
-      <${headingLevel}>${project.title}</${headingLevel}>
-      <img src="${project.image}" alt="${project.title}">
-      <p>${project.description}</p>
-    `;
-containerElement.appendChild(article);
-
-  })
-}
