d766361cf8637bbd21589d4055c25392be992c20
Ankita Inamti <ankita.inamti@gmail.com>
2025-10-25 15:56:54 -0700

    fix

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
 import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
-async function displayLatestProjects() {
-  // Fetch all projects
-  const projects = await fetchJSON('./lib/projects.json');
+const projects = await fetchJSON('./lib/projects.json');
+const latestProjects = projects.slice(0, 3);
+const projectsContainer = document.querySelector('.projects');
 
-  // Take only the first 3
-  const latestProjects = projects.slice(0, 3);
+renderProjects(latestProjects, projectsContainer, 'h2');
 
-  // Select container on homepage
-  const projectsContainer = document.querySelector('.projects');
-
-  // Safety check
-  if (!projectsContainer) {
-    console.error('No .projects container found in index.html');
-    return;
-  }
-
-  // Render first 3 projects
-  renderProjects(latestProjects, projectsContainer, 'h3');
-}
-
-displayLatestProjects();
-
-<script src="index.js" type="module" defer></script>
