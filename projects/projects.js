d8938d8af939d2e6f93c478e7fc17a91dd59d524
Ankita Inamti <ankita.inamti@gmail.com>
2025-10-25 13:04:10 -0700

    counting projects

--- projects/projects.js
+++ projects/projects.js
@@ -6,5 +6,10 @@ const projects = await fetchJSON('../lib/projects.json');
 // Get the container element
 const projectsContainer = document.querySelector('.projects');
 
+// Get the title element and update it with the count
+const projectsTitle = document.querySelector('.projects-title');
+projectsTitle.textContent = `Projects (${projects.length})`;
+
 // Render the projects with h2 headings
 renderProjects(projects, projectsContainer, 'h2');
+
