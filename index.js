558463a1a3b4ddd91684781967c9a4c56d4d9d0f
Ankita Inamti <ankita.inamti@gmail.com>
2025-10-25 18:04:50 -0700

    github display

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ const projectsContainer = document.querySelector('.projects');
 
 renderProjects(latestProjects, projectsContainer, 'h2');
 
+async function displayGitHubStats() {
+  const githubData = await fetchGitHubData('ainamti'); 
+  const profileStats = document.querySelector('#profile-stats');
+
+  if (profileStats) {
+    profileStats.innerHTML = `
+      <h2>GitHub Profile Stats</h2>
+      <dl class="stats-grid">
+        <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+        <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+        <dt>Followers:</dt><dd>${githubData.followers}</dd>
+        <dt>Following:</dt><dd>${githubData.following}</dd>
+      </dl>
+    `;
+  } else {
+    console.warn('No #profile-stats element found in index.html');
+  }
+}
+
+displayGitHubStats();
