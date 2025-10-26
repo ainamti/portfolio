a9231f4a31fe6516ef6141a827696fb5f5b15c02
Ankita Inamti <ankita.inamti@gmail.com>
2025-10-25 19:09:40 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,16 +1,9 @@
 import { fetchJSON, renderProjects } from '../global.js';
-
-// Fetch the projects data
 const projects = await fetchJSON('../lib/projects.json');
-
-// Get the container element
 const projectsContainer = document.querySelector('.projects');
-
-// Get the title element and update it with the count
 const projectsTitle = document.querySelector('.projects-title');
 projectsTitle.textContent = `Projects (${projects.length})`;
 
-// Render the projects with h2 headings
 renderProjects(projects, projectsContainer, 'h2');
 
 
