5dc8a07f61cef34a9653535015a9aab27215e706
Ankita Inamti <ankita.inamti@gmail.com>
2025-11-01 12:42:28 -0700

    Update global.js

--- global.js
+++ global.js
@@ -112,7 +112,10 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
     article.innerHTML = `
       <${headingLevel}>${project.title}</${headingLevel}>
       <img src="${project.image}" alt="${project.title}">
-      <p>${project.description}</p>
+      <div class="project-meta">
+        <p>${project.description}</p>
+        ${project.year ? `<p class="project-year">c. ${project.year}</p>` : ''}
+      </div>
     `;
     containerElement.appendChild(article);
   });
