1f4308d133faa2a656b469da163afd9b89ced7cf
Ankita Inamti <ankita.inamti@gmail.com>
2025-11-01 13:56:04 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -17,6 +17,34 @@ let arc = arcGenerator({
 
 d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
 
+let data = [1, 2];
+
+let total = 0;
+
+for (let d of data) {
+  total += d;
+}
+
+let angle = 0;
+let arcData = [];
+
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
+
+let arcs = arcData.map((d) => arcGenerator(d));
+
+arcs.forEach((arc, idx) => {
+  d3.select('#projects-pie-plot')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors[idx]); 
+});
+
+let colors = ['gold', 'purple'];
+
 
 
 
