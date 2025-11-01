88bf525f81893df00551c64918e8955288cd68e2
Ankita Inamti <ankita.inamti@gmail.com>
2025-11-01 13:13:41 -0700

    projects

--- projects/projects.js
+++ projects/projects.js
@@ -1,10 +1,22 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 const projectsTitle = document.querySelector('.projects-title');
 projectsTitle.textContent = `Projects (${projects.length})`;
 
 renderProjects(projects, projectsContainer, 'h2');
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let arc = arcGenerator({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
+
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+
 
 
 
