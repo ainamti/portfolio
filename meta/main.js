d8644f33a18302c1e300fe3fa01521edb3d2825f
Ankita Inamti <ankita.inamti@gmail.com>
2025-11-08 14:14:27 -0800

    summary

--- meta/main.js
+++ meta/main.js
@@ -1,9 +1,51 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 async function loadData() {
-  const data = await d3.csv('loc.csv');
-  console.log(data);
+  const data = await d3.csv('loc.csv', (row) => ({
+    ...row,
+    line: Number(row.line),
+    depth: Number(row.depth),
+    length: Number(row.length),
+    date: new Date(row.date + 'T00:00' + row.timezone),
+    datetime: new Date(row.datetime),
+  }));
+
   return data;
 }
 
-let data = await loadData();
+function processCommits(data) {
+  return d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+      let first = lines[0];
+      let { author, date, time, timezone, datetime } = first;
+      let ret = {
+        id: commit,
+        url: 'https://github.com/vis-society/lab-7/commit/' + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+      };
+
+      Object.defineProperty(ret, 'lines', {
+        value: lines,
+        enumerable: false,  // hidden in console.log
+        writable: false,
+        configurable: true,
+      });
+
+      return ret;
+    });
+}
+
+async function main() {
+  let data = await loadData();      // <-- load data first
+  let commits = processCommits(data); // <-- then process it
+  console.log(commits);
+}
+
+main(); // run the async functio
